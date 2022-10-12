812a9a5cd84e33d6d670696d44e1663ba39058d0
adelguit <adeelguitoun@gmail.com>
2022-10-12 14:21:44 +0100

    Implement the code for adding scores to the API

---
+++ src/modules/addScore.js
@@ -0,0 +1,20 @@
+const API_URL =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/n0tbY0KgPZQ594I4ehx0/scores/';
+
+// Adding User score to the API.
+const AddScore = async (user, message) => {
+  const response = await fetch(API_URL, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      user: user.name,
+      score: user.score,
+    }),
+  });
+
+  const result = await response.json();
+  message.innerHTML += `<strong>Success!</strong> ${result.result}`;
+  message.classList.add('show');
+};
+
+export default AddScore;
